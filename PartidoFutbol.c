8bb94f9141fd8be758eeb923d9fc50e010541a7a
myroncama12 <myroncama8985@gmail.com>
2018-05-10 20:43:15 -0600

    Controlando semáforo para el balón

--- PartidoFutbol.c
+++ PartidoFutbol.c
@@ -69,25 +69,24 @@ int main(){
             //match_ball->team = 'A';
             //printf("%c\n", match_ball->team);
             
-            for(int i = 0; i < childs; i++){
-                
+            //for(int i = 0; i < childs; i++){
                 if(SEMAPHORES){
-                  // sem_wait(mutex);
+                  sem_wait(mutex);
                    if(process_id <= parent_process_id + 5)
-                    match_ball -> team = 'A';   
-                    else
-                    match_ball -> team = 'B';   
+                     match_ball -> team = 'A';   
+                   else
+                     match_ball -> team = 'B';   
                    match_ball -> pID = process_id;
-                   //printf("la bola pertenece a : %d, del equipo %c \n", match_ball->pID, match_ball->team);
-                   printf("i = %d, %d \n", i, process_id);
+                    printf("la bola pertenece a : %d, del equipo %c \n", match_ball->pID, match_ball->team);
+                   //printf("i = %d, %d \n", i, process_id);
                    sleep(1);
-
+                   sem_post(mutex);
                 }
                 //sleep(2);
                 //sem_post(mutex);
 
                 
-            }
+            //}
             return 0;
         }
     }
